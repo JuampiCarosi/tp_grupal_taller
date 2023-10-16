8ab3bc50ab8155b55c54a2c4d75afdc910203483
JuaniFIUBA <jperezd@fi.uba.ar>
2023-10-16 18:26:19 -0300

    comunicacion puede leer dir de refs

--- src/comunicacion.rs
+++ src/comunicacion.rs
@@ -1,8 +1,8 @@
 use std::net::{TcpListener, TcpStream};
-use std::io::{Write, Read};
+use std::io::{Write, Read, BufRead};
 use std::env;
 use std::path::PathBuf;
-
+use std::fs;
 pub struct Comunicacion { 
     listener: TcpListener,
     stream: TcpStream,
@@ -41,17 +41,17 @@ impl Comunicacion {
         
         Ok(())
     }
-    fn calcular_largo_hex(&self, line: &str) -> String {
+    pub fn calcular_largo_hex(line: &str) -> String {
         let largo = line.len() + 4; // el + 4 es por los 4 bytes que indican el largo
         let largo_hex = format!("{:x}", largo);
         format!("{:0>4}", largo_hex)
     }
 
-    fn parse_line(&self, line: &str) {
+    fn parse_line(&mut self, line: &str) {
         let req: Vec<&str> = line.split_whitespace().collect();
         // veo si es un comando git
        
-        // print!("req: {:?}", req[1]);
+        // print!("req: {:?}", req[0]);
         match req[0] {
             "git-upload-pack" => {
                 println!("git-upload-pack");
@@ -59,9 +59,11 @@ impl Comunicacion {
                 // for i in a {
                 //     println!("i: {:?}", i);
                 // }
-                let mut path = PathBuf::from(self.dir.clone() + args[0]);
+                let mut path = PathBuf::from(self.dir.clone() + args[0] + "refs/");
+                println!("path: {:?}", path);
                 if path.exists() {
-                    self.send_capabilities_refs(&mut path);
+                    self.send_capabilities_refs(&mut path, "heads/");
+                    self.send_capabilities_refs(&mut path, "tags/");
                 } else {
                     println!("no existe el repo");
                 }
@@ -70,19 +72,52 @@ impl Comunicacion {
         }
     }
     
-    fn send_capabilities_refs(&mut self, path: &mut PathBuf) {
-        path.push("info");
-        path.push("refs");
-        path.push("heads");
-        path.push("master");
-        println!("path: {:?}", path);
-        let mut file = std::fs::File::open(path).unwrap();
-        let mut contents = String::new();
-        file.read_to_string(&mut contents).unwrap();
-        println!("contents: {:?}", contents);
-        let mut length_hex = self.calcular_largo_hex(&contents);
-        let mut response = length_hex + &contents;
-        println!("response: {:?}", response);
-        self.stream.write(response.as_bytes()).unwrap();
+    fn send_capabilities_refs(&mut self, path: &mut PathBuf, dir: &str) -> std::io::Result<()> {
+        // let head_ref = self.get_head_ref(path.clone().join("HEAD"));
+        let head_dir = fs::read_dir(path.clone().join(dir))?;
+        for archivo in head_dir {
+            match archivo {
+                Ok(archivo) => {
+                    let path = archivo.path();
+                    // let nombre_archivo = path.file_name().unwrap().to_str().unwrap();
+                    let archivo = std::fs::File::open(path.clone())?; 
+                    let mut contents = String::new();            
+                    std::io::BufReader::new(archivo).read_line(&mut contents)?;
+                    // let metadata = archivo.metadata()?;
+                    // let file_size = metadata.len();
+                    println!("{}", format!("{} {}", contents.trim(), path.to_str().unwrap()));
+                }
+                Err(error) => {
+                    eprintln!("Error leyendo directorio: {}", error);
+                }
+            }
+        }
+        Ok(())
+
+        // path.push("info");
+        // path.push("refs");
+        // path.push("heads");
+        // path.push("master");
+        // println!("path: {:?}", path);
+        // let mut file = std::fs::File::open(path).unwrap();
+        // let mut contents = String::new();
+        // file.read_to_string(&mut contents).unwrap();
+        // println!("contents: {:?}", contents);
+        // let mut length_hex = Self::calcular_largo_hex(&contents);
+        // let mut response = length_hex + &contents;
+        // println!("response: {:?}", response);
+        // self.stream.write(response.as_bytes()).unwrap();
     }
+
+    // fn get_head_ref(&self, path: PathBuf) -> std::io::Result<String> {
+    //     if !path.is_file() {
+    //         return Err(std::io::Error::new(std::io::ErrorKind::NotFound, "No existe HEAD"));
+    //     }
+        
+    //     let file = std::fs::File::open(path)?;
+    //     let mut contents = String::new();            
+    //     std::io::BufReader::new(file).read_line(&mut contents)?;
+    //     let mut head_ref: Vec<&str> = contents.split_whitespace().collect();
+                    
+    // }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::net::TcpStream;
 use std::io::{Write, Read};
-
+use taller::comunicacion::Comunicacion;
 fn main() -> std::io::Result<()> {
     // Configura la dirección del servidor y el puerto
     let server_address = "127.0.0.1:9418"; // Cambia la dirección IP si es necesario
@@ -9,8 +9,10 @@ fn main() -> std::io::Result<()> {
     let mut client = TcpStream::connect(server_address)?;
 
     // Envía datos al servidor (reemplaza esto por tus datos Git)
-    let request_data = b"0045git-upload-pack /schacon/gitbook.git\0host=example.com\0\0version=1\0";
-    client.write_all(request_data)?;
+    let request_data = "git-upload-pack /.git/\0host=example.com\0\0version=1\0";
+    let largo_hex = Comunicacion::calcular_largo_hex(request_data);
+    let a = format!("{}{}", largo_hex, request_data);
+    client.write_all(a.as_bytes())?;
 
     // Recibe la respuesta del servidor (ajusta el tamaño del búfer según tus necesidades)
     // let mut response = [0; 4096];
