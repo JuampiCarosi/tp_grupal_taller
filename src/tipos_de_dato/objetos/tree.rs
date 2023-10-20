9b4f2855789843a396116934fa06b1d6a9562e47
juampicarosi <juampicarosi@gmail.com>
2023-10-20 00:45:37 -0300

    add: fix size de trees

--- src/tipos_de_dato/objetos/tree.rs
+++ src/tipos_de_dato/objetos/tree.rs
@@ -8,41 +8,36 @@ pub struct Tree {
     pub directorio: String,
     pub objetos: Vec<Objeto>,
 }
-
 impl Tree {
     pub fn obtener_tamanio(&self) -> usize {
-        let mut tamanio = 0;
-        for objeto in &self.objetos {
-            tamanio += objeto.obtener_tamanio();
-        }
-        tamanio
+        return Self::mostrar_contenido(&self.objetos).len();
     }
 
     pub fn obtener_hash(&self) -> String {
-        let header = format!("tree {}\0", self.directorio.len());
-        let contenido = Self::mostrar_contenido(self.objetos.clone());
+        let contenido = Self::mostrar_contenido(&self.objetos);
+        let header = format!("tree {}\0", contenido.len());
 
-        let contenido_total = header + &contenido;
+        let contenido_total = format!("{}{}", header, contenido);
+        println!("{}", contenido_total);
+
         let mut hasher = Sha1::new();
-
         hasher.update(contenido_total);
         let hash = hasher.finalize();
-        return format!("{:x}", hash);
+        format!("{:x}", hash)
     }
 
-    fn mostrar_contenido(objetos: Vec<Objeto>) -> String {
+    fn mostrar_contenido(objetos: &Vec<Objeto>) -> String {
         let mut output = String::new();
 
         for objeto in objetos {
             let line = match objeto {
-                Objeto::Blob(blob) => format!("100644 blob {} {}\0", blob.hash, blob.nombre),
+                Objeto::Blob(blob) => format!("100644 {}\0{}", blob.nombre, &blob.hash[..20]),
                 Objeto::Tree(tree) => {
-                    format!("40000 tree {} {}\0", tree.obtener_hash(), tree.directorio)
+                    format!("40000 {}\0{}", tree.directorio, &tree.obtener_hash()[..20])
                 }
             };
             output.push_str(&line);
         }
-
         return output;
     }
 }
@@ -52,26 +47,26 @@ impl Tree {
 mod test {
     use crate::tipos_de_dato::{objeto::Objeto, objetos::tree::Tree};
 
-    // #[test]
-    // fn test_obtener_hash() {
-    //     let objeto = Objeto::from_directorio("test_dir/objetos".to_string()).unwrap();
-    //     let hash = objeto.obtener_hash();
-    //     assert_eq!(hash, "dd167861ef3613e833d23d9519c9e2a79bee25f2");
-    // }
+    #[test]
+    fn test_obtener_hash() {
+        let objeto = Objeto::from_directorio("test_dir/objetos".to_string()).unwrap();
+        let hash = objeto.obtener_hash();
+        assert_eq!(hash, "1442e275fd3a2e743f6bccf3b11ab27862157179");
+    }
 
-    // #[test]
-    // fn test_obtener_tamanio() {
-    //     let objeto = Objeto::from_directorio("test_dir/objetos".to_string()).unwrap();
-    //     let tamanio = objeto.obtener_tamanio();
-    //     assert_eq!(tamanio, 39);
-    // }
+    #[test]
+    fn test_obtener_tamanio() {
+        let objeto = Objeto::from_directorio("test_dir/muchos_objetos".to_string()).unwrap();
+        let tamanio = objeto.obtener_tamanio();
+        assert_eq!(tamanio, 83);
+    }
 
     #[test]
     fn test_mostrar_contenido() {
         let objeto = Objeto::from_directorio("test_dir/objetos".to_string()).unwrap();
 
         if let Objeto::Tree(tree) = objeto {
-            let contenido = Tree::mostrar_contenido(tree.objetos);
+            let contenido = Tree::mostrar_contenido(&tree.objetos);
             assert_eq!(
                 contenido,
                 "100644 blob 2b824e648965b94c6c6b3dd0702feb91f699ed62 archivo.txt\0"
