dbe61bf0b062a5d7075809cf22b7a494cce3aafe
JuaniFIUBA <jperezd@fi.uba.ar>
2023-11-02 09:28:18 -0300

    push restablecido

--- src/tipos_de_dato/comandos/push.rs
+++ src/tipos_de_dato/comandos/push.rs
@@ -15,6 +15,7 @@ use crate::tipos_de_dato::objetos::tree::Tree;
 pub struct Push { 
     hash_refs: HashMap<String, (String, String)>
 }
+
 impl Push { 
     pub fn new() -> Self {
         let mut hash_refs: HashMap<String, (String, String)> = HashMap::new();
@@ -25,9 +26,9 @@ impl Push {
                 (referencia.split(' ').collect::<Vec<&str>>()[0].to_string(), "0".repeat(20)),
             );
         }        
-        println!("hash refs: {:?}", hash_refs)  ;
         Push { hash_refs }
     }
+
     pub fn ejecutar(&mut self) -> Result<String, String> { 
         println!("Se ejecuto el comando push");
         let server_address = "127.0.0.1:9418"; // Cambia la dirección IP si es necesario
@@ -35,18 +36,22 @@ impl Push {
         let mut client = TcpStream::connect(server_address).unwrap();
         let mut comunicacion = Comunicacion::new(client.try_clone().unwrap());
 
-        // si es un push, tengo que calcular los commits de diferencia entre el cliente y el server, y mandarlos como packfiles.
-        // hay una funcion que hace el calculo 
-        // obtener_listas_de_commits
         let request_data = "git-receive-pack /.gir/\0host=example.com\0\0version=1\0"; //en donde dice /.git/ va la dir del repo
         let request_data_con_largo_hex = io::obtener_linea_con_largo_hex(request_data);
 
         client.write_all(request_data_con_largo_hex.as_bytes()).unwrap();
         let mut refs_recibidas = comunicacion.obtener_lineas().unwrap();
+        let mut actualizaciones = Vec::new();
+        let mut objetos_a_enviar  = HashSet::new();
+        if refs_recibidas.is_empty() {
+
+        }
         if !refs_recibidas.is_empty() {
             let first_ref = refs_recibidas.remove(0);
         }
-
+        // ----------------------------------------------------------
+        // no se si esta hecho el caso de los creates, checkear el caso en el que no mandan refs 
+        // ----------------------------------------------------------
         for referencia in &refs_recibidas {
             let obj_id = referencia.split(' ').collect::<Vec<&str>>()[0];
             let referencia = referencia.split(' ').collect::<Vec<&str>>()[1];
@@ -62,20 +67,17 @@ impl Push {
                 None => {
                     // el server tiene un head que el cliente no tiene, abortar push (no borramos brancahes por lo tanto el sv esta)
                 }
-
+                
             }
         }
-        // falta contemplar creado y borrado
-        // cuando es update se manda viejo, nuevo, ref
-        let mut actualizaciones = Vec::new();
-        let mut objetos_a_enviar  = HashSet::new();
         for (key, value) in &self.hash_refs {
-            actualizaciones.push(io::obtener_linea_con_largo_hex(&format!("{} {} {}\n", value.1, value.0, key))); // viejo (el del sv), nuevo (cliente), ref
+            actualizaciones.push(io::obtener_linea_con_largo_hex(&format!("{} {} {}\n", &value.1, &value.0, &key))); // viejo (el del sv), nuevo (cliente), ref
             // checkear que no existan los objetos antes de appendear
-
-            objetos_a_enviar.extend(obtener_commits_y_objetos_asociados(key, &value.1).unwrap());
+    
+            objetos_a_enviar.extend(obtener_commits_y_objetos_asociados(&key, &value.0).unwrap());
         }   
         println!("objetos: {:?}", objetos_a_enviar);
+
         if !actualizaciones.is_empty(){
             comunicacion.responder(actualizaciones).unwrap();
             comunicacion.responder_con_bytes(Packfile::new().obtener_pack_con_archivos(objetos_a_enviar.into_iter().collect(), "./.gir/objects")).unwrap();            
