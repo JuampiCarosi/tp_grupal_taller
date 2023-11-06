56361c3234f0a61918e15f827baf7328d4a9e091
JuaniFIUBA <jperezd@fi.uba.ar>
2023-11-06 13:01:30 -0300

    multi-thread sv

--- src/servidor/servidor_gir.rs
+++ src/servidor/servidor_gir.rs
@@ -11,8 +11,9 @@ fn main() -> Result<(), ()> {
     }
 
     let address = "127.0.0.1:".to_owned() + &argv[1];
-    let mut sv = Servidor::new(&address).unwrap();
-    sv.server_run().unwrap();
+    Servidor::iniciar_servidor(&address).unwrap();
+    // let mut sv = Servidor::new(&address).unwrap();
+    // sv.server_run().unwrap();
     Ok(())
 }
 
