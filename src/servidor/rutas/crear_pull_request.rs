ea998540c8a1547742df55bab427507f2e689232
MateoV <mvroonland@fi.uba.ar>
2023-12-18 15:32:54 -0300

    eliminado de warnings y typo

--- src/servidor/rutas/crear_pull_request.rs
+++ src/servidor/rutas/crear_pull_request.rs
@@ -5,7 +5,7 @@ use crate::{
     tipos_de_dato::{
         http::{
             endpoint::Endpoint, error::ErrorHttp, estado::EstadoHttp, metodos::MetodoHttp,
-            request::Request, response::Response, tipo_contenido::TipoContenido,
+            request::Request, response::Response,
         },
         logger::Logger,
     },
@@ -58,7 +58,7 @@ pub fn guadar_pull_request_acorde_al_numero(
 pub fn responder_pull_request_en_formato_json(
     pull_request: PullRequest,
     logger: Arc<Logger>,
-    estado: EstadoHttp
+    estado: EstadoHttp,
 ) -> Result<Response, ErrorHttp> {
     let body_respuesta = serde_json::to_string(&pull_request).map_err(|e| {
         ErrorHttp::InternalServerError(format!("No se ha podido serializar el pull request: {}", e))
