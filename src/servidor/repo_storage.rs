15a4fd86eb32183d524852f7393f534b1b35e939
Siro Fatala <sirofatala@gmail.com>
2023-12-19 12:02:28 -0300

    Subo el archivo repo_storage

---
+++ src/servidor/repo_storage.rs
@@ -0,0 +1,17 @@
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
+#[derive(Clone)]
+pub struct RepoStorage {
+    pub repo_mutexes: Arc<Mutex<HashMap<String, Arc<Mutex<()>>>>>,
+}
+
+impl RepoStorage {
+    pub fn new() -> Self {
+        RepoStorage {
+            repo_mutexes: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+}
